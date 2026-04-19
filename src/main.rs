d8b8dc5bab48108035a8968bab5c593b330b0105
Dmitry Konishchev <konishchev@gmail.com>
2026-04-19 19:24:01 +0300

    Main module mockup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use clap_verbosity_flag::{Verbosity, InfoLevel};
+use easy_logging::LoggingConfig;
+use log::{Level, trace, debug, info, warn, error};
+
+#[derive(Parser)]
+#[command(about, disable_help_subcommand = true)]
+struct Args {
+    #[arg(short, long, global = true, default_value = "/etc/routectl.yaml", help = "Config path")]
+    config: PathBuf,
+
+    #[command(flatten)]
+    verbose: Verbosity<InfoLevel>,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    #[command(about = "Generates the configuration")]
+    Generate {},
+}
+
+fn main() -> ExitCode {
+    let args = Args::parse();
+
+    let log_level = args.verbose.log_level().unwrap_or(Level::Error);
+    if let Err(err) = LoggingConfig::new(module_path!(), log_level).minimal().build() {
+        let _ = writeln!(io::stderr(), "Failed to initialize the logging: {err}.");
+        return ExitCode::FAILURE;
+    }
+
+    let Err(err) = run(&args) else {
+        return ExitCode::SUCCESS;
+    };
+
+    let message = format!("{err:#}");
+
+    if message.contains('\n') || message.ends_with('.') {
+        error!("{message}");
+    } else {
+        error!("{message}.");
+    }
+
+    ExitCode::FAILURE
+}
+
+fn run(args: &Args) -> Result<()> {
+    match &args.command {
+        Command::Generate {} => {
+            trace!("trace");
+            debug!("debug");
+            info!("info");
+            warn!("warn");
+            error!("error");
+        }
+    }
+    Ok(())
 }
