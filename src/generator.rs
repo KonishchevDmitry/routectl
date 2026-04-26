f2f11bc18f806a5fc56e51ae15b7508f8530c7d2
Dmitry Konishchev <konishchev@gmail.com>
2026-04-26 11:38:51 +0300

    Minor fix

--- src/generator.rs
+++ src/generator.rs
@@ -13,11 +13,13 @@ pub async fn generate(config: &Config) -> Result<()> {
     let resolver = Resolver::new(&config.resolver)?;
 
     stream::iter(&config.rules)
-        .map(|(name, rule)| async {
-            let name = name.to_owned();
-            let ip_stack = rule.ip_stack.unwrap_or(config.ip_stack);
-            process_rule(&name, ip_stack, &rule.targets, &rule.exclude, &resolver).await.with_context(|| format!(
-                "failed to process rule {name:?}"))
+        .map(|(name, rule)| {
+            let resolver = &resolver;
+            async move {
+                let ip_stack = rule.ip_stack.unwrap_or(config.ip_stack);
+                process_rule(name, ip_stack, &rule.targets, &rule.exclude, resolver).await.with_context(|| format!(
+                    "failed to process rule {name:?}"))
+            }
         })
         .buffer_unordered(usize::MAX)
         .try_collect::<Vec<_>>()
